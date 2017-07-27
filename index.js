03e97bd49561a4ec05387a30e1414efe819f84ee
Tuấn Phạm <phamnhungoctuan@gmail.com>
2017-07-27 13:09:22 +0700

    test

--- index.js
+++ index.js
@@ -3,5 +3,5 @@ var app = express();
 app.set('view engine', 'ejs');
 app.set('views', './views');
 app.use(express.static('public'));
-app.listen(3000, () => console.log('Server started 123'))
+app.listen(3000, () => console.log('Server started hehe..'))
 app.get('/', (req, res) => res.render('home'));
